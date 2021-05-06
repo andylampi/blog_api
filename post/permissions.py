2f4805b6d34f2f91d37b721ff7f5e46e374df00b
andy90-star <57332320+andy90-star@users.noreply.github.com>
2021-05-06 17:53:21 +0200

    permission viewsets

--- post/permissions.py
+++ post/permissions.py
@@ -6,3 +6,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
+
+class IsUser(permissions.BasePermission):
+     
+    def has_object_permission(self, request, view, obj):
+        return obj.username == request.user
--- post/views.py
+++ post/views.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
 from .models import Post
 from .serializers import JsonClass, UserJsonClass
-from .permissions import IsAuthorOrReadOnly
+from .permissions import IsAuthorOrReadOnly, IsUser
 from django.contrib.auth import get_user_model
 
 class PostList(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = JsonClass
     
 
 class UserList(viewsets.ModelViewSet):
+    permission_classes = (IsUser, )
     queryset = get_user_model().objects.all()
     serializer_class = UserJsonClass
